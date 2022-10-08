cafe3d42313d6b625b1b79a8333ead29fdb59000
Nikolay Eskov <mr.eskov1@yandex.ru>
2022-10-08 15:23:41 +0300

    [GOHW-1]: Removed unused entities.

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,17 +1,3 @@
 package models
 
-import "time"
-
-type (
-	UserID      int64
-	UserStateID int64
-)
-type User struct {
-	ID    UserID
-	State State
-}
-
-type State struct {
-	ID              UserStateID
-	LastInteraction time.Time
-}
+type UserID int64
